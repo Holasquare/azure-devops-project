fbd30b182cd287233fbaf90cdb5480e672859f89
Ubuntu <azureuser@devops-vm.krdjofvyimgehl5tubhbcrjkuh.xx.internal.cloudapp.net>
2026-05-08 13:56:03 +0000

    updated again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Updated for CI/CD 🚀');
+    res.write('Updated for CI/CD Demo 🚀');
     res.end();
 });
 

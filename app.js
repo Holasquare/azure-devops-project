a8ec0d657254186c8a85f2f67ccbc8f8b0353ea4
Ubuntu <azureuser@devops-vm.krdjofvyimgehl5tubhbcrjkuh.xx.internal.cloudapp.net>
2026-05-08 13:47:16 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello from Azure VM 🚀');
+    res.write('Updated for CI/CD 🚀');
     res.end();
 });
 

f9c9ec0e2f5b5315df3ed08b33129837c0c574dc
Olayinka Olaiya <rachael92.oo@outlook.com@devops-vm.krdjofvyimgehl5tubhbcrjkuh.xx.internal.cloudapp.net>
2026-05-11 15:24:39 +0000

    updated

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Updated via CI/CD 🚀');
+    res.write('Updated via CI/CD New 🚀');
     res.end();
 });
 
+<<<<<<< HEAD
 server.listen(80);
+=======
+server.listen(8080);
+
+>>>>>>> f357a90 (updated)
